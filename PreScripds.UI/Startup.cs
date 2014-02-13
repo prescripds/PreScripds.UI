48b25a7952d487e97e6ae9000833a9544b6cad2f
shreyasbs86 <shreyasbs86@gmail.com>
2014-02-13 18:13:47 +0530

    initial commit

---
+++ PreScripds.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PreScripds.UI.Startup))]
+namespace PreScripds.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
