5896261dc0ba648a7bca2a02307545b9a3e26d5f
shreyasbs86 <shreyasbs86@gmail.com>
2014-02-23 09:55:47 +0530

    added infrastructure components

---
+++ PreScripds.DAL/PreScripdsDb.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PreScripds.Domain;
+
+namespace PreScripds.DAL
+{
+    public class PreScripdsDb : DbContext
+    {
+        static PreScripdsDb()
+        {
+            Database.SetInitializer<PreScripdsDb>(null);
+        }
+        public PreScripdsDb()
+            : base("Name=PreScripdsDb")
+        {
+        }
+
+        public DbSet<Department> departments { get; set; }
+        public DbSet<Role> roles { get; set; }
+        public DbSet<User> users { get; set; }
+
+
+    }
+}
