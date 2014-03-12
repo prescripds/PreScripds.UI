8b5710f65f02971bbcfbc9427e9e7792619a57e9
shreyasbs86 <shreyasbs86@gmail.com>
2014-03-12 21:31:04 +0530

    modified style, RegisterViewModel

--- PreScripds.UI/Models/RegisterViewModel.cs
+++ PreScripds.UI/Models/RegisterViewModel.cs
@@ -9,6 +9,17 @@ namespace PreScripds.UI.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name = "First name")]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Middle name")]
+        public string MiddleName { get; set; }
+
+        [Required]
+        [Display(Name = "Last name")]
+        public string LastName { get; set; }
+
+        [Required]
         [Display(Name = "User name")]
         public string UserName { get; set; }
 
