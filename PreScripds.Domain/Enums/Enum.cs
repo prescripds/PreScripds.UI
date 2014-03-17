2e9e2d1643d93c8bcfba6c2b7f7d73c90d39e766
shreyasbs86 <shreyasbs86@gmail.com>
2014-03-17 22:26:39 +0530

    added data member to gender enum

---
+++ PreScripds.Domain/Enums/Enum.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PreScripds.Domain.Enums
+{
+    public enum Gender
+    {
+        [DataMember]
+        Male = 1,
+        [DataMember]
+        Female = 2,
+        [DataMember]
+        Other = 3
+    }
+}
