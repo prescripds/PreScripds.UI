d6aa49ea57358951744bf26817345f805a0d30f4
shreyasbs86 <shreyasbs86@gmail.com>
2014-04-09 21:39:38 +0530

    added code for adding userrole

---
+++ PreScripds.UI/Models/RoleViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PreScripds.UI.Models
+{
+    public class RoleViewModel
+    {
+        public long RoleId { get; set; }
+        public string RoleName { get; set; }
+        public string RoleDesc { get; set; }
+    }
+}
