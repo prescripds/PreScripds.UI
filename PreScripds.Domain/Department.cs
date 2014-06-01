04e50f441875c340e970f5678d6274cd0d4272e6
shreyasbs86 <shreyasbs86@gmail.com>
2014-06-01 09:46:35 +0530

    Adding file to solution

---
+++ PreScripds.Domain/Department.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace PreScripds.Domain
+{
+    [DataContract(IsReference = true)]
+    public class Department
+    {
+        [DataMember]
+        public int DepartmentId { get; set; }
+        [DataMember]
+        public string DepartmentName { get; set; }
+        [DataMember]
+        public long OrganizationId { get; set; }
+        [DataMember]
+        public string DepartmentDesc { get; set; }
+        [DataMember]
+        public bool IsActive { get; set; }
+        //[DataMember]
+        //public virtual ICollection<UserDepartmentMapping> UserDepartmentMappings { get; set; }
+        [DataMember]
+        public virtual Organization Organization { get; set; }
+    }
+}
